72ae7dd7ddd8c9bd13273d150b857ae02307ac73
dshorter <codesurfer@gmail.com>
2020-02-24 16:08:17 -0500

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,8 +10,8 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  share( productName: string  ) {
+    window.alert('The product ' + productName +  ' has been shared!');
   }
 }
 
